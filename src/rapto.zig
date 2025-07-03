f67d20e9d85077592cdd3f53540a6f78445cd1fc
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2025-07-03 17:48:49 +0200

    zig fmt

--- src/rapto.zig
+++ src/rapto.zig
@@ -277,7 +277,7 @@ pub fn main() void {
     // this allocator is wrapped with tracker Zprof
     const zprof = Zprof.init(&arenaAllocator, DEBUG_MODE_MEMORY) catch signal.OOM();
     defer zprof.deinit();
-    
+
     profiler = &zprof.profiler;
     const allocator = zprof.allocator;
 
