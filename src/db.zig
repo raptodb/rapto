ae82e98616b0dba2ebc5ec1154cdbb85cc20b2df
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2025-07-18 15:23:39 +0200

    added docs, noinling removed, rewrited RENAME complexity and general refactoring

--- src/db.zig
+++ src/db.zig
@@ -47,11 +47,33 @@ const Self = @This();
 
 storage: *Storage,
 
-pub noinline fn SET(self: Self, args: []const u8) !void {
+/// Puts value on head of storage. This function
+/// does not need swapping priority algorithm.
+///
+/// Arguments must be <key> <value>.
+/// The types of values are `integer`, `decimal` and
+/// `string`.
+/// To set integer write the number as value without dot.
+/// To set decimal write the number as value with dot.
+/// To set string write the text encapsulated with "".
+///
+/// Examples:
+/// `SET k 1010` for integers.
+/// `SET k 50.12` for decimals.
+/// `SET k "test"` for strings.
+///
+/// NOTE: if key exists, overwrites it.
+/// Overwriting updates priority and metadata
+/// and can change the type of value.
+///
+/// Complexity O(n)
+pub fn SET(self: Self, args: []const u8) !void {
     @branchHint(.likely);
 
     const key, const value = try utils.kvFormat(args);
 
+    // detects value type from syntax,
+    // parses correlated value and put on store
     switch (try utils.valueTypeFromSerialized(value)) {
         .integer => _ = try self.storage.put(.integer, key, try utils.parseIntegerType(value)),
         .decimal => _ = try self.storage.put(.decimal, key, try utils.parseDecimalType(value)),
@@ -59,7 +81,28 @@ pub noinline fn SET(self: Self, args: []const u8) !void {
     }
 }
 
-pub noinline fn UPDATE(self: Self, args: []const u8) !void {
+/// Increments/decrements `integer` or `decimal` value.
+/// NOTE: `string` is not acceptable.
+///
+/// This function can not change type of value.
+/// To sum value with inserted value write value with
+/// + sign or without sign, to sub, you must write value
+/// with - sign.
+///
+/// Type `integer` can be updated with `integer` or no rem `decimal`.
+/// Type `decimal` can be updated with `integer` or `decimal`
+/// If type is mismatched returns error.
+///
+/// Arguments must be <key> <value>.
+/// Examples:
+/// `UPDATE k 10` (k is integer) if k was 2, now is 12.
+/// `UPDATE k 12.33' (k is decimal) if k was 3.2, now is 15.53.
+/// `UPDATE k -3` (k is integer) if k is 5, now is 2.
+///
+/// Updates priority and metadata.
+///
+/// Complexity O(n)
+pub fn UPDATE(self: Self, args: []const u8) !void {
     @branchHint(.likely);
 
     const key, const string_value = try utils.kvFormat(args);
@@ -77,22 +120,68 @@ pub noinline fn UPDATE(self: Self, args: []const u8) !void {
     obj.metadata.update();
 }
 
-pub noinline fn RENAME(self: Self, args: []const u8) !void {
+/// Renames a key.
+///
+/// Arguments must be <oldkey> <newkey>.
+///
+/// Examples:
+/// `RENAME okey nkey` okey is renamed in nkey.
+///
+/// Updates priority and metadata of oldkey.
+/// If newkey is found (is an error) priority
+/// will be updated.
+///
+/// Complexity O(n+n)
+pub fn RENAME(self: Self, args: []const u8) !void {
     const old_key, const new_key = try utils.kvFormat(args);
 
-    // new key must does not exist
-    if (self.storage.search(new_key) != null) return error.KeyReplacementExist;
+    var new_index: ?u64 = null;
+    var old_index: ?u64 = null;
 
-    if (self.storage.search(old_key)) |i| {
-        const obj = &self.storage.store.items[i];
+    var i: u64 = self.store.items.len;
+    while (i > 0) {
+        i -= 1;
+
+        const obj = &self.store.items[i];
+        if (utils.advancedCompare(obj.key, new_key)) {
+            new_index = i;
+            if (old_index != null) break;
+        } else if (utils.advancedCompare(obj.key, old_key)) {
+            old_index = i;
+            if (new_index != null) break;
+        }
+    }
+
+    // new key must does not exist
+    if (new_index != null) return error.KeyReplacementExist;
+
+    if (old_index) |index| {
+        const obj = &self.storage.store.items[index];
 
         if (obj.key.len != new_key.len)
             obj.key = try self.storage.allocator.realloc(obj.key, new_key.len);
 
         @memcpy(obj.key, new_key);
+        obj.metadata.update();
     } else return error.KeyNotFound;
 }
 
+/// Retrieves value from key.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `GET k`
+/// `GET mykey`
+///
+/// Value type is detected from syntax:
+/// If is `integer` has no dot.
+/// If is `decimal` has dot
+/// If is `string` is encapsulated with "".
+///
+/// Updates priority and metadata.
+///
+/// Complexity O(n)
 pub inline fn GET(self: Self, key: []const u8) !struct { []const u8, bool } {
     @branchHint(.likely);
 
@@ -124,50 +213,118 @@ pub inline fn GET(self: Self, key: []const u8) !struct { []const u8, bool } {
     return .{ slice, false };
 }
 
+/// Returns type of value from key.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `TYPE k`
+/// `TYPE mykey`
+///
+/// Returns `integer`, `decimal` or `string`.
+///
+/// Updates priority.
+///
+/// Complexity O(n)
 pub inline fn TYPE(self: Self, key: []const u8) !struct { []const u8, bool } {
     const obj = self.storage.get(key) orelse return error.KeyNotFound;
     return .{ @tagName(obj.field), false };
 }
 
+/// Checks if key exists.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `CHECK k`
+/// `CHECK mykey`
+///
+/// Returns `0` if key not exists,
+/// else returns `1` if key exists.
+///
+/// Updates priority.
+///
+/// Complexity O(n)
 pub inline fn CHECK(self: Self, key: []const u8) struct { []const u8, bool } {
     return .{ if (self.storage.search(key) == null) "0" else "1", false };
 }
 
+/// Returns the quantity of keys
+/// stored in the database.
+///
+/// This function has no arguments.
+///
+/// Complexity O(1)
 pub inline fn COUNT(self: Self) !struct { []const u8, bool } {
     // use preallocated buffer on stack
     var buf: [20]u8 = undefined;
     return .{ std.fmt.bufPrint(&buf, "{d}", .{self.storage.store.items.len}) catch unreachable, false };
 }
 
-pub noinline fn LIST(self: Self) !struct { []const u8, bool } {
-    var keys = std.ArrayListUnmanaged([]const u8).initCapacity(self.storage.allocator, 0) catch unreachable;
+/// Returns list of keys stored
+/// in the database.
+///
+/// This function has no arguments.
+///
+/// Complexity O(n)
+pub fn LIST(self: Self) !struct { []const u8, bool } {
+    const len = self.storage.store.items.len;
+    if (len == 0) return error.NoKeysFound;
+
+    var keys = std.ArrayListUnmanaged([]const u8).initCapacity(self.storage.allocator, len) catch unreachable;
     defer keys.deinit(self.storage.allocator);
 
     // in order of priority
-    var i: u64 = self.storage.store.items.len;
+    var i: u64 = len;
     while (i > 0) {
         i -= 1;
-        try keys.append(self.storage.allocator, self.storage.store.items[i].key);
+        try keys.appendAssumeCapacity(self.storage.store.items[i].key);
     }
 
-    return if (keys.items.len == 0)
-        error.NoKeysFound
-    else
-        .{ try std.mem.join(self.storage.allocator, " ", keys.items), false };
+    return .{ try std.mem.join(self.storage.allocator, " ", keys.items), false };
 }
 
+/// Updates priority and metadata of key.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `TOUCH k`
+/// `TOUCH mykey`
+///
+/// Complexity O(n)
 pub inline fn TOUCH(self: Self, key: []const u8) !void {
     const obj = self.storage.get(key) orelse return error.KeyNotFound;
     obj.metadata.update();
 }
 
+/// Updates priority moving key
+/// to head with unordered swapping.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `HEAD k`
+/// `HEAD mykey`
+///
+/// Complexity O(2)
 pub fn HEAD(self: Self, key: []const u8) !void {
     const obj = self.storage.get(key) orelse return error.KeyNotFound;
-    const head = &self.storage.store.items[self.storage.store.items.len - 1];
+    const head = &self.storage.store.getLast();
 
     std.mem.swap(Object, obj, head);
 }
 
+/// Updates priority moving key
+/// to tail with unordered swapping.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `TAIL k`
+/// `TAIL mykey`
+///
+/// Complexity O(2)
 pub fn TAIL(self: Self, key: []const u8) !void {
     const obj = self.storage.get(key) orelse return error.KeyNotFound;
     const head = &self.storage.store.items[0];
@@ -175,28 +332,69 @@ pub fn TAIL(self: Self, key: []const u8) !void {
     std.mem.swap(Object, obj, head);
 }
 
+/// Updates priority moving key
+/// to head with ordered method.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `SHEAD k`
+/// `SHEAD mykey`
+///
+/// Complexity O(n)
 pub fn SHEAD(self: Self, key: []const u8) !void {
     const index = self.storage.search(key) orelse return error.KeyNotFound;
     const obj = self.storage.store.orderedRemove(index);
 
     // move to head
-    try self.storage.store.ensureTotalCapacityPrecise(self.storage.allocator, self.storage.store.items.len + 1);
     self.storage.store.insertAssumeCapacity(self.storage.store.items.len, obj);
 }
 
+/// Updates priority moving key
+/// to tail with ordered method.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `STAIL k`
+/// `STAIL mykey`
+///
+/// Complexity O(n)
 pub fn STAIL(self: Self, key: []const u8) !void {
     const index = self.storage.search(key) orelse return error.KeyNotFound;
     const obj = self.storage.store.orderedRemove(index);
 
     // move to tail
-    try self.storage.store.ensureTotalCapacityPrecise(self.storage.allocator, self.storage.store.items.len + 1);
     self.storage.store.insertAssumeCapacity(0, obj);
 }
 
+/// Sorts keys for last access
+/// with insertion sorting.
+///
+/// This function has no arguments.
+///
+/// Complexity O(n²)
 pub inline fn SORT(self: Self) void {
     self.storage.prefetch();
 }
 
+/// Gets or sets access times of a key.
+///
+/// If argument over key is not provided
+/// returns FREQ of key (access times).
+///
+/// Example:
+/// `FREQ key`
+///
+/// If argument <freq> over key is provided
+/// sets key's FREQ with inserted value.
+///
+/// Example:
+/// `FREQ key 40` access times of key is set to 40.
+///
+/// Updates priority.
+///
+/// Complexity O(n)
 pub inline fn FREQ(self: Self, arg: []const u8) !struct { []const u8, bool } {
     var obj: *Object = undefined;
 
@@ -215,6 +413,24 @@ pub inline fn FREQ(self: Self, arg: []const u8) !struct { []const u8, bool } {
     return .{ std.fmt.bufPrint(&buf, "{d}", .{obj.metadata.access_times}) catch unreachable, false };
 }
 
+/// Gets or sets last access of a key.
+///
+/// If argument over key is not provided
+/// returns timestamp of last access
+/// of key.
+///
+/// Example:
+/// `LAST key`
+///
+/// If argument <timestamp> over key is provided
+/// sets key's FREQ with inserted value as timestamp.
+///
+/// Example:
+/// `LAST key 1752500000`
+///
+/// Updates priority.
+///
+/// Complexity O(n)
 pub inline fn LAST(self: Self, arg: []const u8) !struct { []const u8, bool } {
     var obj: *Object = undefined;
 
@@ -233,6 +449,18 @@ pub inline fn LAST(self: Self, arg: []const u8) !struct { []const u8, bool } {
     return .{ std.fmt.bufPrint(&buf, "{d}", .{obj.metadata.last_access}) catch unreachable, false };
 }
 
+/// Returns the latency between
+/// now and the last access.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `IDLE k`
+/// `IDLE mykey`
+///
+/// Updates priority.
+///
+/// Complexity O(n)
 pub inline fn IDLE(self: Self, key: []const u8) !struct { []const u8, bool } {
     const obj = self.storage.get(key) orelse return error.KeyNotFound;
     const idle = std.math.sub(
@@ -246,6 +474,23 @@ pub inline fn IDLE(self: Self, key: []const u8) !struct { []const u8, bool } {
     return .{ std.fmt.bufPrint(&buf, "{d}", .{idle}) catch unreachable, false };
 }
 
+/// Returns how much memory
+/// in bytes occupies field.
+///
+/// If type of field is `integer`
+/// or `decimal` returns always 8.
+///
+/// Useful for get th length of `string`.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `LEN k`
+/// `LEN mykey`
+///
+/// Updates priority.
+///
+/// Complexity O(n)
 pub inline fn LEN(self: Self, key: []const u8) !struct { []const u8, bool } {
     const obj = self.storage.get(key) orelse return error.KeyNotFound;
     const size = if (obj.field == .string) obj.field.string.len else 8;
@@ -255,6 +500,22 @@ pub inline fn LEN(self: Self, key: []const u8) !struct { []const u8, bool } {
     return .{ std.fmt.bufPrint(&buf, "{d}", .{size}) catch unreachable, false };
 }
 
+/// Returns how much memory
+/// in bytes occupies object.
+///
+/// Occupied memory is composed
+/// by 56 (min space of struct)
+/// + key size + field size
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `SIZE k`
+/// `SIZE mykey`
+///
+/// Updates priority.
+///
+/// Complexity O(n)
 pub inline fn SIZE(self: Self, key: []const u8) !struct { []const u8, bool } {
     const obj = self.storage.get(key) orelse return error.KeyNotFound;
 
@@ -267,6 +528,25 @@ pub inline fn SIZE(self: Self, key: []const u8) !struct { []const u8, bool } {
     return .{ std.fmt.bufPrint(&buf, "{d}", .{size}) catch unreachable, false };
 }
 
+/// Gets or sets memory profiling
+/// info of heap.
+///
+/// Arguments must be <action>.
+///
+/// Actions (get):
+/// live: gets live bytes.
+/// peak: gets peak of bytes.
+/// total: gets total bytes allocated.
+/// alloc: gets alloc count.
+/// free: gets dealloc count.
+///
+/// Actions (set):
+/// reset-peak: reset peak bytes to 0.
+/// reset-total: reset total bytes allocated to 0.
+/// reset-count: reset count of allocations.
+/// and deallocations to 0.
+///
+/// Complexity O(1)
 pub inline fn MEM(self: Self, profiler: *Profiler, arg: []const u8) !struct { []const u8, bool } {
     _ = self;
 
@@ -299,32 +579,64 @@ pub inline fn MEM(self: Self, profiler: *Profiler, arg: []const u8) !struct { []
     return .{ std.fmt.bufPrint(&buf, "{d}", .{value}) catch unreachable, false };
 }
 
-pub noinline fn DB(self: Self, arg: []const u8) !struct { []const u8, bool } {
-    return if (utils.advancedCompare(arg, "name")) blk: {
-        break :blk .{ self.storage.conf.name.?, false };
-    }
+/// Gets database infos.
+///
+/// Arguments must be <action>.
+///
+/// Actions:
+/// name: gets name of database.
+/// size: gets how much memory keys occupy.
+/// cap: gets the capacity of database,
+/// if capacity is not set, returns ALLRAM.
+///
+/// Complexity O(n)
+pub fn DB(self: Self, arg: []const u8) !struct { []const u8, bool } {
+    return if (utils.advancedCompare(arg, "name"))
+        .{ self.storage.conf.name.?, false }
+    else
     // block for preallocated
     // buffer on stack
-    else blk: {
+    blk: {
         // use preallocated buffer on stack
         var buf: [20]u8 = undefined;
 
         break :blk if (utils.advancedCompare(arg, "cap"))
-            .{ std.fmt.bufPrint(&buf, "{d}", .{self.storage.conf.db_size.?}) catch unreachable, false }
+            if (self.storage.conf.db_size.?) |size|
+                .{ std.fmt.bufPrint(&buf, "{d}", .{size}) catch unreachable, false }
+            else
+                .{ "ALLRAM", false }
         else if (utils.advancedCompare(arg, "size"))
-            .{ std.fmt.bufPrint(&buf, "{d}", .{self.storage.conf.db_size.? - self.storage.store_cap}) catch unreachable, false }
+            .{ std.fmt.bufPrint(&buf, "{d}", .{std.math.maxInt(u64) - self.storage.store_cap}) catch unreachable, false }
         else
             error.UnknownArgument;
     };
 }
 
-pub noinline fn DUMP(self: Self, key: []const u8) !struct { []const u8, bool } {
+/// Returns serialized key from
+/// key in bytes format.
+///
+/// Arguments must be <key>.
+///
+/// Updates priority and metadata.
+///
+/// Complexity O(n)
+pub fn DUMP(self: Self, key: []const u8) !struct { []const u8, bool } {
     var obj = self.storage.get(key) orelse return error.KeyNotFound;
     obj.metadata.update();
     return .{ try obj.serialize(self.storage.allocator), true };
 }
 
-pub noinline fn RESTORE(self: Self, obj: []const u8) !void {
+/// Puts key into database from
+/// serialized key.
+///
+/// Arguments must be <serialized>.
+///
+/// NOTE: if key exists, overwrites it.
+/// Overwriting updates priority and metadata
+/// and can change the type of value.
+///
+/// Complexity O(n)
+pub fn RESTORE(self: Self, obj: []const u8) !void {
     const d = Object.deserialize(self.storage.allocator, obj) catch return error.InvalidObject;
 
     const i = switch (d.field) {
@@ -336,7 +648,12 @@ pub noinline fn RESTORE(self: Self, obj: []const u8) !void {
     self.storage.store.items[i].metadata = d.metadata;
 }
 
-pub noinline fn ERASE(self: Self) !void {
+/// Removes all keys from database.
+///
+/// This function has no arguments.
+///
+/// Complexity O(n)
+pub fn ERASE(self: Self) !void {
     var i: usize = self.storage.store.items.len;
     while (i > 0) {
         i -= 1;
@@ -344,6 +661,15 @@ pub noinline fn ERASE(self: Self) !void {
     }
 }
 
+/// Removes key from database.
+///
+/// Arguments must be <key>.
+///
+/// Examples:
+/// `DEL k`
+/// `DEL mykey`
+///
+/// Complexity O(n)
 pub inline fn DEL(self: Self, key: []const u8) !void {
     @branchHint(.likely);
 
@@ -351,16 +677,32 @@ pub inline fn DEL(self: Self, key: []const u8) !void {
     try self.storage.removeAtIndex(index);
 }
 
+/// Saves a snapshot of database
+/// in persistent file.
+///
+/// This function has no arguments.
+///
+/// Complexity O(n)
 pub inline fn SAVE(self: Self, logger: *log.Logger) !void {
     snap.snap(self.storage, logger, false) catch return error.SaveFailed;
 }
 
-pub noinline fn COPY(self: Self, args: []const u8) !void {
+/// Copies key to another key.
+///
+/// Arguments must be <srckey> and <dstkey>.
+///
+/// NOTE: if dst key exist, overwrites it.
+///
+/// Examples:
+/// `COPY key key1`
+///
+/// Complexity O(n+n)
+pub fn COPY(self: Self, args: []const u8) !void {
     const key, const dst = try utils.kvFormat(args);
     const rawkey, const heap_allocated = try self.DUMP(key);
     defer if (heap_allocated) self.storage.allocator.free(rawkey);
 
-    var d = Object.deserialize(self.storage.allocator, rawkey) catch return error.InvalidObject;
+    var d = Object.deserialize(self.storage.allocator, rawkey) catch unreachable;
     d.key = try self.storage.allocator.dupe(u8, dst);
 
     const i = switch (d.field) {
