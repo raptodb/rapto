ad797b1be497dc457d7b9d223b73ffb2f2a1dd13
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2025-09-07 15:10:24 +0200

    fixed error message frontend

--- src/ree.zig
+++ src/ree.zig
@@ -70,7 +70,7 @@ pub fn expandResolveError(err: ResolveError) []const u8 {
         error.NoKeysFound => "ERR: no keys found.",
         error.UnknownArgument => "ERR: invalid argument.",
         error.ExcedeedSpaceLimit => "ERR: excedeed db space limit.",
-        error.NoPersistence => "Persistence disabled from server.",
+        error.NoPersistence => "ERR: persistence disabled from server.",
         else => unreachable,
     };
 }
