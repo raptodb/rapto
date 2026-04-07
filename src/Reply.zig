617b9f4ce63116c6439fa49880f1792b6900e1b1
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2026-04-08 01:09:49 +0200

    refactor and improve of control flow

--- src/Reply.zig
+++ src/Reply.zig
@@ -5,26 +5,36 @@
 //! This file is part of "Rapto".
 //! It contains the implementation of reply builder and task processor.
 
+/// Reply builder by processing Task(s).
+const Reply = @This();
+
 const std = @import("std");
 const status = @import("Reply/status.zig");
 const dispatcher = @import("Reply/dispatcher.zig");
+const assert = std.debug.assert;
 
 const Memory = @import("Memory.zig");
 const Module = @import("Reply/Module.zig");
 const Task = @import("Task.zig");
-const Reply = @This();
 
-const HeaderType: type = u32;
+pub const Config = struct {
+    /// Minimum size of Allocating. This optimizes the
+    /// allocation/deallocation overhead.
+    /// Static size is allocated on initialization time
+    /// and never deallocated until `.deinit()` method.
+    static_size: u32 = 16 * 1024,
+};
 
 allocating: std.Io.Writer.Allocating,
 memory: *Memory,
-static_size: u32,
 
-pub fn init(memory: *Memory, static_size: u32) error{OutOfMemory}!Reply {
+config: Config,
+
+pub fn init(memory: *Memory, config: Config) error{OutOfMemory}!Reply {
     return .{
-        .allocating = try .initCapacity(memory.allocator, static_size),
+        .allocating = try .initCapacity(memory.allocator, config.static_size),
         .memory = memory,
-        .static_size = static_size,
+        .config = config,
     };
 }
 
@@ -33,59 +43,71 @@ pub fn deinit(self: *Reply) void {
 }
 
 /// Processes a task and builds the reply.
-/// The returned slice is valid until the next call to `process`.
-pub fn process(self: *Reply, task: *const Task) error{ OutOfMemory, Shutdown }![]const u8 {
+/// The returned slice is valid until the next call to this function.
+pub fn processTask(self: *Reply, task: *const Task) error{ OutOfMemory, Shutdown }![]const u8 {
     self.clearAndShrink();
     const module: Module = .init(self.memory, &self.allocating.writer);
 
     var iterator = task.iterator();
     while (iterator.next()) |maybe_query| {
-        const header_offset = try self.reserveHeader(HeaderType);
+        const header_offset = self.reserveHeader(u32) catch return error.OutOfMemory;
 
-        const maybe_error = if (maybe_query) |query| blk: {
-            var mut_query = query;
-            break :blk dispatcher.dispatch(module, &mut_query);
-        } else |err| blk: {
+        var mut_query = maybe_query catch |err| {
             // parsing errors should be handled by
             // the client, thus this branch is cold
             @branchHint(.cold);
-            break :blk status.write(module.writer, status.fromParseError(err));
+            status.write(module.writer, status.fromParseError(err)) catch return error.OutOfMemory;
         };
 
         // if catches an error, it is considered a fatal error
-        maybe_error catch |err| switch (err) {
-            inline error.OutOfMemory,
-            error.Shutdown,
-            => |e| e,
-            // assuming always writer of module is derived
-            // by std.Io.Writer.Allocating, an error about
-            // write is always OOM
-            error.WriteFailed => error.OutOfMemory,
-        };
+        try processQuery(module, &mut_query);
 
-        const length = module.writer.end - header_offset - @sizeOf(HeaderType);
-        self.writeOffset(HeaderType, @intCast(length), header_offset);
+        const length = module.writer.end - header_offset - @sizeOf(u32);
+        self.writeOffset(u32, @intCast(length), header_offset);
     }
 
     return self.allocating.written();
 }
 
+/// Wrapper for `.dispatch()` to handle and cast errors.
+fn processQuery(module: Module, query: *Task.Query) error{ OutOfMemory, Shutdown }!void {
+    const maybe_error = dispatcher.dispatch(module, query);
+
+    // if catches an error, it is considered a fatal error
+    maybe_error catch |err| return switch (err) {
+        inline error.OutOfMemory,
+        error.Shutdown,
+        => |e| e,
+        // assuming always writer of module is derived
+        // by std.Io.Writer.Allocating, an error about
+        // write is always OOM
+        error.WriteFailed => error.OutOfMemory,
+    };
+}
+
 fn reserveHeader(self: *Reply, comptime T: type) error{WriteFailed}!u64 {
     const header_offset = self.allocating.writer.end;
-    self.allocating.writer.writeInt(T, 0, .little);
+    try self.allocating.writer.writeInt(T, 0, .little);
     return header_offset;
 }
 
 fn clearAndShrink(self: *Reply) void {
     const allocator = self.allocating.allocator;
     var list = self.allocating.toArrayList();
-    if (list.capacity > self.static_size) list.shrinkAndFree(allocator, self.static_size);
-    self.allocating = .fromArrayList(allocator, &list);
-    self.allocating.clearRetainingCapacity();
+    defer self.allocating = .fromArrayList(allocator, &list);
+
+    if (list.capacity > self.config.static_size) {
+        list.shrinkAndFree(allocator, self.config.static_size);
+    }
+    list.clearRetainingCapacity();
 }
 
 fn writeOffset(self: *Reply, comptime T: type, value: T, offset: u64) void {
     const writer = self.allocating.writer;
+
+    assert(writer.buffer.len >= @sizeOf(T));
+    assert(writer.buffer.len >= offset + @sizeOf(T));
+
     std.mem.writeInt(
         T,
         writer.buffer[offset .. offset + @sizeOf(T)][0..@sizeOf(T)],
