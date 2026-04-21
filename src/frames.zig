0dca31fda5a8c8946894754371a02d62095b68a7
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2026-04-21 19:16:29 +0200

    improve flexibility of frames.zig

--- src/frames.zig
+++ src/frames.zig
@@ -12,52 +12,64 @@
 const std = @import("std");
 const assert = std.debug.assert;
 
-pub const framePrefixType = u32;
+pub const Iterator = IteratorType(u32);
+pub const ExtendedIterator = IteratorType(u64);
 
-pub const Iterator = struct {
-    reader: std.Io.Reader,
+pub const Builder = BuilderType(u32);
+pub const ExtendedBuilder = BuilderType(u64);
 
-    pub fn init(frames: []const u8) Iterator {
-        return .{ .reader = .fixed(frames) };
-    }
+pub fn IteratorType(comptime PrefixType: type) type {
+    return struct {
+        const Self = @This();
 
-    pub fn next(self: *Iterator) ?[]const u8 {
-        const len = self.reader.takeInt(framePrefixType, .little) catch return null;
-        return self.reader.take(len) catch return null;
-    }
-};
+        reader: std.Io.Reader,
 
-pub const Builder = struct {
-    writer: *std.Io.Writer,
-    begin_offset: u64,
+        pub fn init(frames: []const u8) Self {
+            return .{ .reader = .fixed(frames) };
+        }
 
-    pub fn begin(writer: *std.Io.Writer) error{WriteFailed}!Builder {
-        const header_offset = writer.end;
-        try writer.writeInt(framePrefixType, 0, .little);
-        return .{ .writer = writer, .begin_offset = header_offset };
-    }
+        pub fn next(self: *Self) ?[]const u8 {
+            const len = self.reader.takeInt(PrefixType, .little) catch return null;
+            return self.reader.take(len) catch return null;
+        }
+    };
+}
 
-    pub fn end(self: *Builder) void {
-        assert(self.writer.buffer.len >= @sizeOf(framePrefixType));
-        assert(self.writer.buffer.len >= self.begin_offset + @sizeOf(framePrefixType));
-        assert(self.writer.end >= self.begin_offset);
+pub fn BuilderType(comptime PrefixType: type) type {
+    return struct {
+        const Self = @This();
 
-        const prefix_size = @sizeOf(framePrefixType);
-        const size_from_begin = self.writer.end - self.begin_offset - prefix_size;
+        writer: *std.Io.Writer,
+        begin_offset: u64,
 
-        assert(size_from_begin <= std.math.maxInt(framePrefixType));
+        pub fn begin(writer: *std.Io.Writer) error{WriteFailed}!Self {
+            const header_offset = writer.end;
+            try writer.writeInt(PrefixType, 0, .little);
+            return .{ .writer = writer, .begin_offset = header_offset };
+        }
 
-        const ptr_buf =
-            self.writer.buffer[self.begin_offset .. self.begin_offset + prefix_size].ptr;
+        pub fn end(self: *Self) void {
+            assert(self.writer.buffer.len >= @sizeOf(PrefixType));
+            assert(self.writer.buffer.len >= self.begin_offset + @sizeOf(PrefixType));
+            assert(self.writer.end >= self.begin_offset);
 
-        std.mem.writeInt(
-            framePrefixType,
-            ptr_buf[0..prefix_size],
-            @intCast(size_from_begin),
-            .little,
-        );
-    }
-};
+            const prefix_size = @sizeOf(PrefixType);
+            const size_from_begin = self.writer.end - self.begin_offset - prefix_size;
+
+            assert(size_from_begin <= std.math.maxInt(PrefixType));
+
+            const ptr_buf =
+                self.writer.buffer[self.begin_offset .. self.begin_offset + prefix_size].ptr;
+
+            std.mem.writeInt(
+                PrefixType,
+                ptr_buf[0..prefix_size],
+                @intCast(size_from_begin),
+                .little,
+            );
+        }
+    };
+}
 
 test "frames" {
     const allocator = std.testing.allocator;
