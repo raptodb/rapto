4739f78b7697a9cc0ba6c9ee2aca6ea90f17a485
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2026-04-29 22:40:32 +0200

    improve task design and add serialize methods

--- src/Task.zig
+++ src/Task.zig
@@ -5,7 +5,8 @@
 //! This file is part of "Rapto".
 //! It contains the implementation of task.
 
-/// Indipendent operation for dispatch. It contains
+/// Indipendent collection of operations for
+/// the StateMachine execution. It contains
 /// pipeline of queries and its metadata.
 const Task = @This();
 
@@ -14,30 +15,20 @@ const frames = @import("frames.zig");
 
 pub const Query = @import("Task/Query.zig");
 
+/// Microsecond timestamp used to track when the task was
+/// executed. This indicator is useful also for unique id.
+timestamp: u64,
 /// Pipeline containing the raw queries to be executed.
 pipeline: []const u8,
-/// Timestamp used to track when the task was executed.
-/// This indicator is useful also for unique id.
-timestamp: u64,
-/// Callback function to send reply to source of the Task.
-callbackFn: ?CallbackFnType,
 
-const CallbackFnType = *const fn (ctx: *anyopaque, data: []const u8) anyerror!void;
-
-/// Initializes a task with the given reply callback function and pipeline.
-/// The timestamp is set to the current time in nanoseconds,
+/// Initializes a task with pipeline and timestamp.
+/// The timestamp is expressed in microseconds,
 /// useful for tracking execution time and to identify tasks.
-/// This function assumes ownership of the pipeline.
-pub fn init(pipeline: []const u8, timestamp: std.Io.Timestamp, callbackFn: ?CallbackFnType) Task {
+pub fn init(pipeline: []const u8, timestamp: std.Io.Timestamp) Task {
     return .{
         .pipeline = pipeline,
         .timestamp = @intCast(timestamp.toMicroseconds()),
-        .callbackFn = callbackFn,
     };
-}
-
-pub fn deinit(self: *const Task, allocator: std.mem.Allocator) void {
-    allocator.free(self.pipeline);
 }
 
 /// Iterator of queries.
@@ -53,6 +44,41 @@ pub const Iterator = struct {
 
 pub fn iterator(self: *const Task) Iterator {
     return .{ .wrapped_iterator = .init(self.pipeline) };
+}
+
+const Hasher = std.hash.crc.Crc32Iscsi;
+
+pub fn serializeToWriter(self: *const Task, writer: *std.Io.Writer) error{WriteFailed}!void {
+    const hasher_instance: Hasher = .init();
+    var hashed_writer: std.Io.Writer.Hashed(Hasher) = .initHasher(
+        writer,
+        hasher_instance,
+        &.{},
+    );
+
+    try hashed_writer.writer.writeInt(u64, self.timestamp, .little);
+    try hashed_writer.writer.writeAll(self.pipeline);
+    try hashed_writer.writer.writeInt(u32, hashed_writer.hasher.final(), .little);
+}
+
+pub fn deserialize(buf: []u8) error{ ReadFailed, InvalidFormat, ChecksumMismatch }!Task {
+    if (buf.len < 4) return error.InvalidFormat;
+
+    const raw_task = buf[0 .. buf.len - 4];
+
+    const taken_hash = Hasher.hash(raw_task);
+    const hash: u32 = @bitCast(buf[buf.len - 4 ..][0..4].*);
+    if (hash != taken_hash) return error.ChecksumMismatch;
+
+    var reader: std.Io.Reader = .fixed(raw_task);
+
+    const timestamp = reader.takeInt(u64, .little) catch |err| return switch (err) {
+        error.EndOfStream => error.InvalidFormat,
+        error.ReadFailed => error.ReadFailed,
+    };
+    const pipeline = reader.buffer[reader.seek..];
+
+    return .{ .timestamp = timestamp, .pipeline = pipeline };
 }
 
 test "Task" {
@@ -84,8 +110,8 @@ test "Task" {
     }
 
     const pipeline = try allocator.dupe(u8, writer.buffered());
-    const task: Task = .init(pipeline, std.Io.Timestamp.now(io, .awake), null);
-    defer task.deinit(allocator);
+    const task: Task = .init(pipeline, std.Io.Timestamp.now(io, .awake));
+    defer allocator.free(pipeline);
 
     try std.testing.expect(task.timestamp != 0);
 
@@ -101,4 +127,18 @@ test "Task" {
     }
 
     try std.testing.expectEqual(null, it.next());
+
+    allocating.clearRetainingCapacity();
+
+    try task.serializeToWriter(writer);
+    const serialized = writer.buffered();
+    const task_clone: Task = try .deserialize(serialized);
+
+    it = task.iterator();
+    var it2 = task_clone.iterator();
+    while (it.next()) |maybe_expected| {
+        var expected = try maybe_expected;
+        const a = try it2.next().?;
+        try std.testing.expect(expected.isEqualTo(&a));
+    }
 }
