afa73f3a10cd80a408e747aae52c010c67d66217
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2026-05-08 12:42:48 +0200

    remove unused import

--- src/field/collection/Map.zig
+++ src/field/collection/Map.zig
@@ -17,7 +17,6 @@ const field = @import("../../field.zig");
 
 const ScalarItem = @import("../scalar.zig").ScalarItem;
 const String = @import("../scalar.zig").String;
-const List = @import("../collection.zig").List;
 
 pub const MapContext = struct {
     pub fn hash(_: @This(), s: KeyString) u64 {
