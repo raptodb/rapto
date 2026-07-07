cedb8f36dfee999e29548c6a673ea3979f917d2e
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2026-07-07 15:24:12 +0200

    refactor build.zig

--- build.zig
+++ build.zig
@@ -9,14 +9,17 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
     const exe = b.addExecutable(.{
         .name = "raptodb",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = .ReleaseFast,
-        }),
+        .root_module = exe_mod,
     });
     exe.root_module.link_libc = true;
 
@@ -30,7 +33,6 @@ pub fn build(b: *std.Build) void {
     });
     lib_unit_tests.root_module.link_libc = true;
 
-    // add step for testing
     const run_tests = b.addRunArtifact(lib_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_tests.step);
