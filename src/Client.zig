a0df747a1c339b105802a7c4c5763ec766318eee
Andrea Vaccaro <vaccaro.andrea45@gmail.com>
2026-09-03 14:50:30 +0200

    Client: add cursor and lock

--- src/Client.zig
+++ src/Client.zig
@@ -97,6 +97,8 @@ pub const Batch = struct {
     pipeline: Pipeline,
     /// Queries appended but not flushed.
     pending: u64 = 0,
+    /// For each query, sets flag `assume_lock_ownership` to true.
+    assume_lock_ownership: bool = false,
 
     pub fn deinit(self: *Batch) void {
         self.pipeline.deinit();
@@ -156,7 +158,10 @@ pub const Batch = struct {
         const args_info = @typeInfo(@TypeOf(args));
         comptime assert(args_info == .@"struct");
 
-        var builder: Builder = try .begin(&self.pipeline, command, flags);
+        var mut_flags = flags;
+        mut_flags.assume_lock_ownership = .init(self.assume_lock_ownership);
+
+        var builder: Builder = try .begin(&self.pipeline, command, mut_flags);
         defer builder.end();
 
         const fields = args_info.@"struct".fields;
@@ -473,6 +478,366 @@ pub const Batch = struct {
             .cursor = .init(config.cursor),
         };
         return self.build(.entries_patterns, flags, .{ key, glob_patterns });
+    }
+
+    /// Tries to lock atomically keys until `unlock`. If any key is
+    /// already locked, it fails throwing error.Locked.
+    /// Initializes lock instance that assumes all queries appended do not include
+    /// non-owned keys or locked keys by others. Initialization assumes no pending
+    /// queries. Ownership will be transfered to caller, responsible of `unlock()`.
+    ///
+    /// After this call, the batch used to perform lock operations can be accessed
+    /// directly from Lock.batch, not responsible of `deinit()`.
+    pub fn lock(
+        self: *Batch,
+        allocator: std.mem.Allocator,
+        io: std.Io,
+        keys: []const []const u8,
+        config: Lock.Config,
+    ) Lock.Error!Lock {
+        return .lock(self, allocator, io, .{ .literals = keys }, config);
+    }
+
+    /// As `lock`, but keys are locked/unlocked by searching keys
+    /// with glob patterns. Initialization assumes no pending queries.
+    /// Ownership will be transfered to caller, responsible of `unlock()`.
+    ///
+    /// After this call, the batch used to perform lock operations can be accessed
+    /// directly from Lock.batch, not responsible of `deinit()`.
+    pub fn lockMatching(
+        self: *Batch,
+        allocator: std.mem.Allocator,
+        io: std.Io,
+        glob_patterns: []const []const u8,
+        config: Lock.Config,
+    ) Lock.Error!Lock {
+        return .lock(self, allocator, io, .{ .glob_patterns = glob_patterns }, config);
+    }
+
+    /// Cursor is used by iterative operations, so `flush()`
+    /// is handled automatically. Any iterative function of cursor,
+    /// asserts no pending queries. Does not take ownership.
+    pub fn cursor(self: *Batch) Cursor {
+        return .{ .b = self };
+    }
+};
+
+pub const Lock = struct {
+    pub const Config = struct {
+        /// Minimum size for pipeline of lock batch. This optimizes
+        /// the allocation/deallocation overhead.
+        /// The preserved size is allocated at initialization time
+        /// and is never deallocated until the `deinit()` method.
+        batch_pipeline_preserved_size: u32 = 4 * 1024,
+        /// Maximum readable bytes from `flush()` or `flushOne()` over header.
+        /// This avoid too large inputs from reader (maybe socket)
+        /// throwing error.StreamTooLong.
+        /// For now, by default, we can set at largest limit possible,
+        /// see Pipeline.Header.
+        max_pipeline_bytes: u64 = std.math.maxInt(u32),
+    };
+
+    pub const Error = Batch.FlushError || error{Locked};
+
+    /// Likely to be accessed directly. This batch sets
+    /// by default `assume_lock_ownership` to true.
+    batch: Batch,
+
+    b: *Batch,
+    locked: bool,
+    keys: union(enum) {
+        literals: []const []const u8,
+        glob_patterns: []const []const u8,
+    },
+
+    fn lock(
+        b: *Batch,
+        allocator: std.mem.Allocator,
+        io: std.Io,
+        keys: union(enum) {
+            literals: []const []const u8,
+            glob_patterns: []const []const u8,
+        },
+        config: Lock.Config,
+    ) Lock.Error!Lock {
+        assert(b.pending == 0);
+
+        var lock_instance: Lock = undefined;
+        lock_instance.b = b;
+        lock_instance.keys = keys;
+
+        const batch_config: Batch.Config = .{
+            .pipeline_preserved_size = config.batch_pipeline_preserved_size,
+            .max_pipeline_bytes = config.max_pipeline_bytes,
+        };
+        lock_instance.batch = try b.client.batch(allocator, batch_config);
+        errdefer lock_instance.batch.deinit();
+        lock_instance.batch.assume_lock_ownership = true;
+
+        // Try to lock all selected keys.
+        try b.build(.lock, .{}, .{keys});
+        const resp = try b.flushOne(io);
+        // At least one key is locked...
+        // We can't lock an already locked key.
+        if (resp.maybeError(.locked)) return error.Locked;
+
+        lock_instance.locked = true;
+        return lock_instance;
+    }
+
+    /// Unlocks all locked keys/patterns. This call flushes query.
+    /// Assumes no pending queries from all two batches.
+    pub fn unlock(self: *Lock, io: std.Io) Batch.FlushError!void {
+        assert(self.locked);
+        assert(self.b.pending == 0 and self.batch.pending == 0);
+
+        switch (self.keys) {
+            .literals => |l| try self.b.build(.unlock, .{}, .{l}),
+            .glob_patterns => |p| try self.b.build(.unlock_patterns, .{}, .{p}),
+        }
+        const rv = try self.b.flushOne(io);
+        // Unlock never returns an error.
+        assert(!rv.hasError());
+
+        try self.batch.deinit();
+        // In this line flush has succeeded.
+        self.locked = false;
+        self.b.assume_lock_ownership = false;
+    }
+};
+
+pub const Cursor = struct {
+    b: *Batch,
+
+    pub fn ListResultIterator(comptime Context: type) type {
+        return struct {
+            const Self = @This();
+
+            b: *Batch,
+
+            ctx: Context,
+            queryFn: *const fn (
+                ctx: *const Context,
+                b: *Batch,
+                config: Batch.MatchingCursorConfig,
+            ) std.mem.Allocator.Error!void,
+
+            cursor: u64 = 0,
+            count: u64,
+            max_cursor: u64,
+
+            pub fn next(self: *Self, io: std.Io) Batch.FlushOneError!?value.ListIterator {
+                assert(self.b.pending == 0);
+                if (self.cursor == self.max_cursor) return null;
+
+                const config: Batch.MatchingCursorConfig = .{
+                    .limit = .init(self.count),
+                    .cursor = self.cursor,
+                };
+                try self.queryFn(self.ctx, self.b, config);
+
+                const rv = try self.b.flushOne(io);
+                if (rv.type() != .list) return error.MismatchType;
+                const list = rv.list;
+
+                self.cursor = @min(self.cursor +| self.count, self.max_cursor);
+                return list;
+            }
+        };
+    }
+
+    pub fn IntegerResultIterator(comptime Context: type) type {
+        return struct {
+            const Self = @This();
+
+            b: *Batch,
+
+            ctx: Context,
+            queryFn: *const fn (
+                ctx: *const Context,
+                b: *Batch,
+                config: Batch.MatchingCursorConfig,
+            ) std.mem.Allocator.Error!void,
+
+            cursor: u64 = 0,
+            count: u64,
+            max_cursor: u64,
+
+            pub fn next(self: *Self, io: std.Io) Batch.FlushOneError!?i64 {
+                assert(self.b.pending == 0);
+
+                const config: Batch.MatchingCursorConfig = .{
+                    .limit = .init(self.count),
+                    .cursor = self.cursor,
+                };
+                try self.queryFn(self.ctx, self.b, config);
+
+                const rv = try self.b.flushOne(io);
+                if (rv.type() != .integer) return error.MismatchType;
+
+                self.cursor = @min(self.cursor +| self.count, self.max_cursor);
+                return rv.scalar.integer;
+            }
+        };
+    }
+
+    const KeysContext = struct { glob_patterns: []const []const u8 };
+    const EntriesContext = struct { key: []const u8, glob_patterns: []const []const u8 };
+
+    pub fn keysIterator(
+        self: Cursor,
+        glob_patterns: []const []const u8,
+        count: u64,
+        /// Once `next()` reaches this position, it returns null instead of
+        /// issuing another query. Maybe retrieved with `Batch.count(.{})`.
+        max_cursor: u64,
+    ) ListResultIterator(KeysContext) {
+        return .{
+            .b = self.b,
+            .ctx = .{ .glob_patterns = glob_patterns },
+            .queryFn = keysMatchingFn,
+            .count = count,
+            .max_cursor = max_cursor,
+        };
+    }
+
+    pub fn entriesIterator(
+        self: Cursor,
+        key: []const u8,
+        glob_patterns: []const []const u8,
+        count: u64,
+        /// Once `next()` reaches this position, it returns null instead of
+        /// issuing another query. Maybe retrieved with `Batch.countEntries(.{})`.
+        max_cursor: u64,
+    ) ListResultIterator(EntriesContext) {
+        return .{
+            .b = self.b,
+            .ctx = .{
+                .key = key,
+                .glob_patterns = glob_patterns,
+            },
+            .queryFn = entriesMatchingFn,
+            .count = count,
+            .max_cursor = max_cursor,
+        };
+    }
+
+    pub fn countIterator(
+        self: Cursor,
+        glob_patterns: []const []const u8,
+        count: u64,
+        /// Once `next()` reaches this position, it returns null instead of
+        /// issuing another query. Maybe retrieved with `Batch.count(.{})`.
+        max_cursor: u64,
+    ) IntegerResultIterator(KeysContext) {
+        return .{
+            .b = self.b,
+            .ctx = .{ .glob_patterns = glob_patterns },
+            .queryFn = countMatchingFn,
+            .count = count,
+            .max_cursor = max_cursor,
+        };
+    }
+
+    pub fn countEntriesIterator(
+        self: Cursor,
+        key: []const u8,
+        glob_patterns: []const []const u8,
+        count: u64,
+        /// Once `next()` reaches this position, it returns null instead of
+        /// issuing another query. Maybe retrieved with `Batch.countEntries(.{})`.
+        max_cursor: u64,
+    ) IntegerResultIterator(EntriesContext) {
+        return .{
+            .b = self.b,
+            .ctx = .{ .key = key, .glob_patterns = glob_patterns },
+            .queryFn = countEntriesMatchingFn,
+            .count = count,
+            .max_cursor = max_cursor,
+        };
+    }
+
+    pub fn delIterator(
+        self: Cursor,
+        glob_patterns: []const []const u8,
+        count: u64,
+        /// Once `next()` reaches this position, it returns null instead of
+        /// issuing another query. Maybe retrieved with `Batch.count(.{})`.
+        max_cursor: u64,
+    ) IntegerResultIterator(KeysContext) {
+        return .{
+            .b = self.b,
+            .ctx = .{ .glob_patterns = glob_patterns },
+            .queryFn = delMatchingFn,
+            .count = count,
+            .max_cursor = max_cursor,
+        };
+    }
+
+    pub fn delEntriesIterator(
+        self: Cursor,
+        key: []const u8,
+        glob_patterns: []const []const u8,
+        count: u64,
+        /// Once `next()` reaches this position, it returns null instead of
+        /// issuing another query. Maybe retrieved with `Batch.countEntries(.{})`.
+        max_cursor: u64,
+    ) IntegerResultIterator(EntriesContext) {
+        return .{
+            .b = self.b,
+            .ctx = .{ .key = key, .glob_patterns = glob_patterns },
+            .queryFn = delEntriesMatchingFn,
+            .count = count,
+            .max_cursor = max_cursor,
+        };
+    }
+
+    fn keysMatchingFn(
+        ctx: *const KeysContext,
+        b: *Batch,
+        config: Batch.MatchingCursorConfig,
+    ) std.mem.Allocator.Error!void {
+        return b.keysMatching(ctx.glob_patterns, config);
+    }
+
+    fn entriesMatchingFn(
+        ctx: *const EntriesContext,
+        b: *Batch,
+        config: Batch.MatchingCursorConfig,
+    ) std.mem.Allocator.Error!void {
+        return b.entriesMatching(ctx.key, ctx.glob_patterns, config);
+    }
+
+    fn countMatchingFn(
+        ctx: *const KeysContext,
+        b: *Batch,
+        config: Batch.MatchingCursorConfig,
+    ) std.mem.Allocator.Error!void {
+        return b.countMatching(ctx.glob_patterns, config);
+    }
+
+    fn countEntriesMatchingFn(
+        ctx: *const EntriesContext,
+        b: *Batch,
+        config: Batch.MatchingCursorConfig,
+    ) std.mem.Allocator.Error!void {
+        return b.countEntriesMatching(ctx.key, ctx.glob_patterns, config);
+    }
+
+    fn delMatchingFn(
+        ctx: *const KeysContext,
+        b: *Batch,
+        config: Batch.MatchingCursorConfig,
+    ) std.mem.Allocator.Error!void {
+        return b.delMatching(ctx.glob_patterns, config);
+    }
+
+    fn delEntriesMatchingFn(
+        ctx: *const EntriesContext,
+        b: *Batch,
+        config: Batch.MatchingCursorConfig,
+    ) std.mem.Allocator.Error!void {
+        return b.delEntriesMatching(ctx.key, ctx.glob_patterns, config);
     }
 };
 
